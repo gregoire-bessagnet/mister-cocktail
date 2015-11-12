8626178038c2fd687ce362449c8de52daab21b45
Gregoire Bessagnet <gregoire.bessagnet@inbox.com>
2015-11-12 14:05:59 +0100

    doses controller

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,4 +1,7 @@
 class DosesController < ApplicationController
+
+  before_action :find_cocktail, only: [ :new, :create ]
+
   def index
   end
 
@@ -6,11 +9,28 @@ class DosesController < ApplicationController
   end
 
   def new
+    @dose = Dose.new
   end
 
   def create
+    @dose = @cocktail.doses.build(dose_params)
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
   def destroy
   end
 end
+
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description)
+  end
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
