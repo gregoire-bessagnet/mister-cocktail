3c159493fe192271a6afe50af3622071cf808680
Gregoire Bessagnet <gregoire.bessagnet@inbox.com>
2015-11-13 11:29:30 +0100

    added css

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -3,7 +3,7 @@ class Dose < ActiveRecord::Base
   belongs_to :ingredient
 
   validates :description, presence: true
-  validates :cocktail, presence: true
-  validates :ingredient, presence: true
+  validates :cocktail, :ingredient, presence: true
+  validates_uniqueness_of :cocktail_id, :scope => [:ingredient_id]
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'cocktails#index'
 
-  resources :cocktails do
+  resources :cocktails, only: [:show, :index, :new, :create] do
     resources :doses, only: [:new, :create, :destroy]
   end
 
